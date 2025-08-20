95e33938c02cddff7c6192d59c5337e37d1fbd82
WangZhenghao-star <2946627423@qq.com>
2025-08-20 16:44:27 +0800

    Add files via upload
    Supplementary Material
    The data folder contains all the datasets used in the experiment. Among them, 768_1.csv is the standardized dataset for Pima Indian diabetes, LMCH_.csv is the standardized dataset for Iraqi diabetes, and 2011-2020_1 is the standardized dataset from the National Health and Nutrition Examination Survey (NHANES).
    All codes involved in the experiment have been implemented. The experimental codes are located in the "test" folder, and the other folders contain tool codes.

---
+++ 第三章code/test/demo_real_data_LMCH.py
@@ -0,0 +1,36 @@
+import torch
+from recode.utils import output_adj
+from recode.gae import GAE
+import pandas as pd
+import numpy as np
+from recode.causal_strength import adj_cs
+
+# 加载数据集，确保第一行被识别为列名
+X = pd.read_csv("../data/dataset/LMCH_1.csv")
+
+# 检查列名，确认没有意外的字符串
+print(X.columns)
+
+# 如果'CLASS'列存在，并且您想要删除它
+if 'CLASS' in X.columns:
+    X = X.drop(columns=['CLASS'])
+else:
+    print("列 'CLASS' 不存在。")
+
+# 将DataFrame转换为NumPy数组，并确保数据类型为float32
+X_np = X.values.astype(np.float32)  # 将数据转换为NumPy数组
+
+# 初始化GAE模型
+gae = GAE(epochs=10, device_type="cpu")
+
+# 训练模型
+gae.learn(X_np)  # 直接传递NumPy数组
+
+# 输出因果关系邻接矩阵
+print(gae.causal_matrix)
+
+# 输出因果强度邻接矩阵（如果需要）
+print(adj_cs(gae.causal_matrix, X_np))
+
+# 使用recode.utils中的output_adj函数输出邻接矩阵
+output_adj(gae.causal_matrix)
